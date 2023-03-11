fff13acf057fb12053f33829cc5b21206be60978
Fulufhelo-M <fulu.mariba@gmail.com>
2023-03-11 20:21:20 +0200

    game is now keeping score

--- R_P_S.py
+++ R_P_S.py
@@ -8,21 +8,44 @@ valid_choice = ('rock','paper','scissors')
 beats_vc = ('scissors','rock','paper')
 run = True
 
-pc_int = random.randint(0,2)
-pc_pick = valid_choice[pc_int]
-
 try:
-    while run:
-        player_pick = input("\nEnter Your Pick (Bye to exit): \n").lower()
-    
-        if pc_pick == player_pick:
-            message = f"\nComputer picked {pc_pick}.\n\nIt's a draw!\n"
-        elif valid_choice.index(player_pick) == beats_vc.index(pc_pick):
-            message = f"\nComputer picked {pc_pick}.\n\nYou won!\n"
-        elif valid_choice.index(player_pick) != beats_vc.index(pc_pick):
-            message = f"\nComputer picked  {pc_pick}.\n\nYou lost!\n"
-        print(message)
+    while run:   
+        pc_wins = 0
+        player_wins = 0
+        draws = 0
+
+        games_to_play = int(input("\nLet's play!! Best of?: "))
+
+        for x in range(0,(games_to_play)):
+            pc_int = random.randint(0,2)
+            pc_pick = valid_choice[pc_int]
+
+            player_pick = input("\nEnter Your Pick (Bye to exit): \n").lower()
+            if pc_pick == player_pick:
+                message = f"\nComputer picked {pc_pick}.\n\nIt's a draw!\n"
+                draws += 1
+            if valid_choice.index(player_pick) == beats_vc.index(pc_pick):
+                message = f"\nComputer picked {pc_pick}.\n\nYou won!\n"
+                player_wins += 1
+            if valid_choice.index(pc_pick) == beats_vc.index(player_pick):
+                message = f"\nComputer picked  {pc_pick}.\n\nYou lost!\n"
+                pc_wins += 1
+            print(message)
+
+        if  pc_wins > player_wins:
+             winner = f"\nPC won {pc_wins} out of {games_to_play} games\nTotal draws = {draws}\nBetter Luck Next Time"
+        if  pc_wins < player_wins:
+             winner = f"\nPlayer won {player_wins} out of {games_to_play} games\nTotal draws = {draws}\nYou Crunshed It"
+        if  pc_wins == player_wins:
+             winner = f"\nPC won {pc_wins} and Player won {player_wins} out of {games_to_play} games\nTotal draws = {draws}\nYou've met your match it seems"
+        print(winner)
+
+        keep_playing = input("\nKeep playing (Y/N)?: ").lower()
+        if keep_playing == 'y':
+            run = True
+        if keep_playing == 'n':
+            run = False
+            print("\nBye :)\n")
+
 except:
-    run = False
-    message = "Bye :)\n"
-    print(message)
+    print("\nSomething went wrong. Reload game.\n")
