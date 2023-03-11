93a2bda287d378c917076292abb1753a27d1884a
Fulufhelo-M <fulu.mariba@gmail.com>
2023-03-11 18:58:24 +0200

    game can now be played on a loop until player chooses to terminate

--- R_P_S.py
+++ R_P_S.py
@@ -6,21 +6,23 @@ print("...scissors...")
 
 valid_choice = ('rock','paper','scissors')
 beats_vc = ('scissors','rock','paper')
+run = True
+
 pc_int = random.randint(0,2)
 pc_pick = valid_choice[pc_int]
 
 try:
-    player_pick = input("\nEnter Your Pick (Bye to exit): \n").lower()
-
-    if player_pick == 'bye':
-        message = "Goodbye :)"
-    elif pc_pick == player_pick:
-        message = f"\nComputer picked {pc_pick}.\n\nIt's a draw!\n"
-    elif valid_choice.index(player_pick) == beats_vc.index(pc_pick):
-        message = f"\nComputer picked {pc_pick}.\n\nYou won!\n"
-    else:
-        message = f"\nComputer picked  {pc_pick}.\n\nYou lost!\n"
-except: 
-    message = "Oops. Try again."
-
-print(message)
+    while run:
+        player_pick = input("\nEnter Your Pick (Bye to exit): \n").lower()
+    
+        if pc_pick == player_pick:
+            message = f"\nComputer picked {pc_pick}.\n\nIt's a draw!\n"
+        elif valid_choice.index(player_pick) == beats_vc.index(pc_pick):
+            message = f"\nComputer picked {pc_pick}.\n\nYou won!\n"
+        elif valid_choice.index(player_pick) != beats_vc.index(pc_pick):
+            message = f"\nComputer picked  {pc_pick}.\n\nYou lost!\n"
+        print(message)
+except:
+    run = False
+    message = "Bye :)\n"
+    print(message)
